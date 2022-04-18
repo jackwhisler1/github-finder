26db885cdd69c97767deb9b4e31a7a43ac4b22ad
Jack Whisler <whisler1@gmail.com>
2022-04-18 16:49:57 -0500

    creates navbar component, tests out React functionality in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
+import React, { Component } from "react";
+import Navbar from "./components/navbar";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello From React</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = "Jack ";
+    const loading = false;
+    const foo = () => "Bar";
+    const showName = true; 
+
+    if (loading) {
+      return <h2>Loading...</h2>;
+    }
+
+    return (
+      <div className="App">
+        <Navbar/>
+        {loading ? (
+          <h2>Loading . . . </h2>
+        ) : (
+          <h1>Hello {showName && name}</h1>
+          // ternary operator to show name
+        )}
+        <h1>Hello {foo()}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class navbar extends Component {
+  render() {
+    return (
+      <nav className="navbar bg-primary">
+        <h1>Navbar</h1>
+      </nav>
+    );
+  }
+}
+
+export default navbar;
